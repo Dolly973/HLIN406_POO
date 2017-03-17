31a035329b1201a29091368290b2b10f4f98be27
Dolly973 <valide.dolores@gmail.com>
2017-03-17 15:28:46 +0100

    Update CatalogueHashtable.java

--- src/CatalogueHashtable.java
+++ src/CatalogueHashtable.java
@@ -1,10 +1,21 @@
-public class Catalogue{
-private HashMap<String,NoticeBiblio> listeNotices = new Hashmap<>();
-//ajouter une notice
-public void ajoute (NoticeBiblio not)
-{listeNotice.put(not.getiSBN(),not);}
-//Recherche
-public NoticeBiblio recherche(String isbn)
-{ returnlisteNotice.get(isbn);}
-//si on veut faire un "parcours total" ex: recherche a partir d'un nom d'auteur >>keyset collection desclefs >>values collections des valeurs
+package TP5;
+
+import java.util.HashMap;
+
+public class CatalogueHashtable {
+	private HashMap<String,NoticeBibliographique> listeNotice = new HashMap<>();
+
+	public CatalogueHashtable() {
+		;
+	}
+
+	//ajouter une notice
+	public void ajoute (NoticeBibliographique not)
+	{listeNotice.put(not.getISBN(),not);}
+	//Recherche
+	public NoticeBibliographique recherche(String isbn)
+	{ return listeNotice.get(isbn);}
+	//si on veut faire un "parcours total" ex: recherche a partir d'un nom d'auteur >>keyset collection des clefs >>values collections des valeurs
 }
+
+
