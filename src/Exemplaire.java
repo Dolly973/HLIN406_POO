ff0513b03fcb834fe89ad840cd351a39894b4bf7
Dolly973 <valide.dolores@gmail.com>
2017-03-17 15:29:39 +0100

    Update Exemplaire.java

--- src/Exemplaire.java
+++ src/Exemplaire.java
@@ -1,21 +1,55 @@
 package TP5;
 
+import java.util.ArrayList;
+
 public class Exemplaire {
-	private boolean empruntE;
+	private boolean emprunt;
 	private Etat etat;
 	private boolean enReparation;
+	private ArrayList <Abonne> abonne;
 
+	//Constructeur
 	public Exemplaire(){
-		this.empruntE = null != null ;
-		this.enReparation = null != null;
+		this.emprunt = false;
+		this.enReparation =false;
 		this.etat = null;
 	}
 	public Exemplaire( boolean emp, Etat e, boolean eR){
-		this.empruntE= emp;
+		this.emprunt= emp;
 		this.enReparation = eR;
 		this.etat = e;
 	}
-public boolean emprunte(){
+	
+	//Accesseurs
+	public boolean isEmprunt() {
+		return emprunt;
+	}
+	public void setEmprunt(boolean emprunt) {
+		this.emprunt = emprunt;
+	}
+	public Etat getEtat() {
+		return etat;
+	}
+	public void setEtat(Etat etat) {
+		this.etat = etat;
+	}
+	public boolean isEnReparation() {
+		return enReparation;
+	}
+	public void setEnReparation(boolean enReparation) {
+		this.enReparation = enReparation;
+	}
+	public ArrayList<Abonne> getAbonne() {
+		return abonne;
+	}
+	public void setAbonne(ArrayList<Abonne> abonne) {
+		this.abonne = abonne;
+	}
+	
+	
+	
+	//Methodes
+public boolean emprunte(Abonne a){
 	return true;
 }
 public boolean rendre(){
@@ -31,3 +65,4 @@ public boolean mettreAJour(){
 	return false;
 }
 }
+
